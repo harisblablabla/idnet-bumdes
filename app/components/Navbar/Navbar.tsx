682a996a0a6271f2399c79d47ff615b1ecedef3f
haris pc kantor <haris.fadhilah21@gmail.com>
2024-09-20 18:34:17 +0700

    feat: title landing page

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -3,34 +3,34 @@ import React from 'react'
 import LanguageSwitcher from './LanguageSwitcher'
 
 function Navbar() {
-  return (
-    <div className="navbar bg-white ">
-          {/* mobile */}
-          <div className="navbar-start lg:hidden"></div>
+    return (
+        <div className="navbar bg-white ">
+            {/* mobile */}
+            <div className="navbar-start lg:hidden"></div>
 
-           {/* web */}
-           <div className="navbar-start hidden lg:flex pl-10">
-           <ul className="menu menu-horizontal px-1 text-slate-600 font-medium z-50 ">
-           <li className=''><Link href={'https://daftar.ideanet.net.id/helpdesk/ticket-form'}>Beranda</Link></li>
-           <li className=''><Link href={'https://daftar.ideanet.net.id/helpdesk/ticket-form'}>Layanan</Link></li>
-           <li className=''><Link href={'https://daftar.ideanet.net.id/helpdesk/ticket-form'}>Tentang Perusahaan</Link></li>
-           <li className=''><Link href={'https://daftar.ideanet.net.id/helpdesk/ticket-form'}>Kontak Kami</Link></li>
-           </ul>
-           </div>
+            {/* web */}
+            <div className="navbar-start hidden lg:flex pl-10">
+                <ul className="menu menu-horizontal px-1 text-slate-600 font-medium z-50 ">
+                    <li className=''><Link href={'https://daftar.ideanet.net.id/helpdesk/ticket-form'}>Beranda</Link></li>
+                    <li className=''><Link href={'https://daftar.ideanet.net.id/helpdesk/ticket-form'}>Layanan</Link></li>
+                    <li className=''><Link href={'https://daftar.ideanet.net.id/helpdesk/ticket-form'}>Tentang Perusahaan</Link></li>
+                    <li className=''><Link href={'https://daftar.ideanet.net.id/helpdesk/ticket-form'}>Kontak Kami</Link></li>
+                </ul>
+            </div>
 
-           <div className="navbar-end hidden lg:flex pr-16">
-           <ul className="menu menu-horizontal text-slate-600 font-medium z-50">
+            <div className="navbar-end hidden lg:flex pr-16">
+                <ul className="menu menu-horizontal text-slate-600 font-medium z-50">
                     <li className='relative'>
                         <button>id</button>
-                            {/* <ul className="absolute top-12 right-2 menu w-56 bg-base-200 rounded-box shadow-[0_0_10px_3px_rgba(229,229,229)]">
+                        {/* <ul className="absolute top-12 right-2 menu w-56 bg-base-200 rounded-box shadow-[0_0_10px_3px_rgba(229,229,229)]">
                                 <LanguageSwitcher />
                             </ul> */}
                     </li>
                 </ul>
                 <Link href={"/daftar"} className="btn btn-primary btn-md">Login</Link>
-           </div>
-    </div>
-  )
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,17 @@ export default function Home() {
   return (
     <main className="min-h-screen flex-col justify-center">
       <Navbar />
-      
+      <div className="sm:container sm:mb-20 sm:mx-auto">
+        <div className="flex flex-row">
+          <div className="flex flex-col">
+            <h1 className='font-semibold text-4xl mb-5 text-gray-900'>Desa lebih mudah dikelola, warga lebih sejahtera</h1>
+            <p className="mb-5 text-neutral-700">Optimalkan pengelolaan desa Anda dengan solusi digital dari IdeaNet Smart Village. Kami menyediakan berbagai alat dan layanan untuk memudahkan manajemen desa. sehingga warga dapat menikmati layanan yang lebih baik dan kehidupan yang lebih sejahtera.</p>
+          </div>
+          <div className="flex-row">
+            <h3>ahmad bai</h3>
+          </div>
+        </div>
+      </div>
       <h3>huadhuahdu</h3>
     </main>
   )
